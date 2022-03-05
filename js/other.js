261a4b6aa520fb304be160ae412bd8fef17b88ae
HeHe <1049965797@qq.com>
2022-03-05 17:28:11 +0800

    init

---
+++ js/other.js
@@ -0,0 +1,43 @@
+const otherArr = [
+  {
+    id: 1,
+    show: true,
+    categoryName: '',
+    children: [
+      { id: 1, name: '计划', href: './view/life.html'},
+      { id: 2, name: '简历', href: './view/resume/fullScroll.html'},
+      { id: 3, name: '简历pdf', href: './view/resume/resumepdf.html'},
+      { id: 44, name: 'power', href: './view/power.html'},
+    ]
+  },
+  {
+    id: 3,
+    show: false,
+    categoryName: 'echart',
+    children: [
+      { id: 10, name: 'wrok', href: './view/echarts/echarts-work.html'},
+      { id: 11, name: 'momey', href: './view/echarts/echarts-money.html'},
+      { id: 12, name: 'life', href: './view/echarts/echarts-life.html'},
+    ]
+  },
+  {
+    id: 4,
+    show: false,
+    categoryName: 'tools',
+    children: [
+      { id: 5, name: '日历', href: './view/日历.html'},
+      { id: 8, name: '轮播', href: './view/scroll/index.html'},
+      { id: 9, name: 'time', href: './view/time.md'},
+    ]
+  },
+  {
+    id: 2,
+    show: false,
+    categoryName: '',
+    children: [
+      { id: 6, name: '水果', href: './view/health/vc.html'},
+      { id: 7, name: '健康', href: './view/health/health.html'},
+      
+    ]
+  },
+];
---
+++ vc.js
@@ -0,0 +1,67 @@
+const OtherModel = [
+  { id: 1, name: 'plan', href: './other/plan.html'},
+  { id: 2, name: 'vc', href: './other/vc.html' },
+  { id: 2, name: 'vcsss', href: './other/vc.md' },
+  // { id: 2, name: 'health', href: './other/vc.html', detail: '人体所必需的营养素有蛋白质、脂类、糖类、维生素、水和无机盐（矿物质）、膳食纤维（纤维素）7类' },
+  //
+] 
+
+// 橘（mandarin）蓝莓(blueberry) 梅子(plum) 桃子（peach）红心果(fig) 牛油果(avocado) 芒果（mango）仙人果(Prickly Pear) 甜瓜(melon) 香蕉（banana）刺角瓜(kiwano) 葡萄柚(grapefruit) 椰子（coconut） 番木瓜（papaya） 菠萝（）
+const Fruit = [
+  {id: 1, categoryName: '春（叁、肆、伍）', categoryNumber: [3,4,5], bg: 'bg-72c4b8', children: [
+      {id: 0, name: '菠萝',}, 
+      {id: 1, name: '山竹', en: 'mangosteen'}, 
+      {id: 2, name: '芒果',}, 
+      {id: 3, name: '桑葚',}, 
+      {id: 4, name: '草莓', en: 'strawberry'}, 
+      {id: 5, name: '荔枝', en: 'litchi'}, 
+      {id: 6, name: '樱桃', en: 'cherry'}, 
+      { id: 7, name: '青枣', },
+    ]
+  },
+  {
+    id: 2, categoryName: '夏（陆、柒、捌）', categoryNumber: [6,7,8], bg: 'bg-green1', children: [
+      { id: 0, name: '黄皮', },
+      { id: 1, name: '枇杷',}, 
+      { id: 2, name: '桃子', describe: '五果之首'}, 
+      { id: 3, name: '西瓜', en: 'watermelon'}, 
+      { id: 4, name: '柠檬',}, 
+      { id: 5, name: '李子',}, 
+      { id: 6, name: '百香果', en: 'passion fruit'}, 
+      { id: 7, name: '杏',}, 
+      { id: 8, name: '榴莲',}, 
+      { id: 9, name: '哈密瓜',}, 
+      { id: 10, name: '菠萝蜜',}, 
+      { id: 11, name: '火龙果', en: 'dragonfruit'}, 
+      { id: 12, name: '莲雾', },
+      { id: 13, name: '杨梅', },
+      
+    ]
+  },
+  {
+    id: 3, categoryName: '秋（玖、拾、冬）', categoryNumber: [9,10,11], bg: 'bg-fbe24', children: [
+      {id: 0, name: '葡萄', en: 'grapes'}, 
+      {id: 1, name: '梨', en: 'pear', describe: '百果之宗'}, 
+      {id: 2, name: '柚子',}, 
+      {id: 3, name: '柿子',}, 
+      {id: 4, name: '山楂',}, 
+      {id: 5, name: '核桃',}, 
+      {id: 6, name: '石榴', en: 'pomegranate'}, 
+    ]
+  },
+  {
+    id: 4, categoryName: '冬（腊、壹、贰）', categoryNumber: [12,1,2], bg: 'bg-blue1', children: [
+      {id: 0, name: '番石榴（芭乐）', en: 'guava', detail: '7.9/100g糖，低糖高钾水果'}, 
+      { id: 1, name: '苹果', en: 'apple'}, 
+      { id: 2, name: '橙子', en: 'orange'}, 
+      { id: 3, name: '猕猴桃', en: 'kiwi fruit'}, 
+      { id: 4, name: '甘蔗',}, 
+      { id: 5, name: '柚子', en: 'pomelo'}, 
+      { id: 6, name: '雪莲果',}, 
+    ]
+  },
+
+
+
+]
+
