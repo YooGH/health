d175328f0a9ab924374f5533e34e271a303bd048
HeHe <1049965797@qq.com>
2022-07-16 15:28:48 +0800

    fmt1.2

---
+++ fmt/js/data.js
@@ -0,0 +1,10 @@
+const clothesList = [
+  { id: 1, name: 'clothes1', img: './img/1.jpg', details: '', price: '6,88.00'},
+  { id: 2, name: 'clothes2', img: './img/2.jpg', details: '', price: '7,99.00'},
+  { id: 3, name: 'clothes3', img: './img/3.jpg', details: '', price: '4,44.00'},
+  { id: 4, name: 'clothes4', img: './img/4.jpg', details: '', price: '1,99.00'},
+  { id: 5, name: 'clothes5', img: './img/5.jpg', details: '', price: '8,99.00'},
+  { id: 6, name: 'clothes6', img: './img/6.jpg', details: '', price: '1,99.00'},
+  { id: 7, name: 'clothes7', img: './img/7.jpg', details: '', price: '1,88.00'},
+  { id: 8, name: 'clothes8', img: './img/8.jpg', details: '', price: '7,66.00'},
+]
